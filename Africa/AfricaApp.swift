d0e714f09044065f76abc280b50f50a28088eb16
Datafox_JCP <datafox@outlook.com>
2020-10-22 08:10:03 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Juan Carlos on 22/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
