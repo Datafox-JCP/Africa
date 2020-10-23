8c98fa0c12e2e6807f0b6fb0ac35c43c378b9c66
Datafox_JCP <datafox@outlook.com>
2020-10-23 08:17:21 -0500

    Decode JSON file and fetch data

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Juan Carlos on 22/10/20.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
